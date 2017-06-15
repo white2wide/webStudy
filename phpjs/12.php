1be6f7127fccb92b15de434b105979142a93579a
Juhong Baek <juhong@Juhongs-MacBook-Pro.local>
2017-06-15 10:37:44 +0900

    Function

---
+++ phpjs/12.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script>
+      function a() {
+        document.write("hello JS Function");
+      }
+      a();
+    </script>
+    <h1>php</h1>
+    <?php
+      function a() {
+        echo "hello PHP Function";
+      }
+      a();
+     ?>
+  </body>
+</html>
