3b042f51ba9c143b1ac5428a181445d24bb90080
Juhong Baek <juhong@adsl-172-10-9-130.dsl.sndg02.sbcglobal.net>
2017-06-27 20:21:10 +0900

    Library1

--- write.php
+++ write.php
@@ -1,6 +1,7 @@
 <?php
-$conn = mysqli_connect("localhost", "root", 1111);
-mysqli_select_db($conn, "opentutorials");
+require("config/config.php");
+require("lib/db.php");
+$conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
 $result = mysqli_query($conn, "SELECT * FROM topic");
 ?>
 
