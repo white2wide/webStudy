5186136ef881da3f9168734fee34446914924571
Juhong Baek <juhong@adsl-172-10-9-130.dsl.sndg02.sbcglobal.net>
2017-06-27 16:31:14 +0900

    Security

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
     <ol>
       <?php
       while ($row = mysqli_fetch_assoc($result)) {
-        echo '<li><a href="http://localhost:8080/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+        echo '<li><a href="http://localhost:8080/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
       }
       ?>
     </ol>
@@ -35,9 +35,9 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
       $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
       $result = mysqli_query($conn, $sql);
       $row = mysqli_fetch_assoc($result);
-      echo '<h2>'.$row['title'].'</h2>';
-      echo '<p>'.$row['name'].'</p>';
-      echo $row['description'];
+      echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+      echo '<p>'.htmlspecialchars($row['name']).'</p>';
+      echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
     }
     ?>
   </article>
---
+++ phpjs/14.php
@@ -0,0 +1,26 @@
+<?php
+$conn = mysqli_connect("localhost", "root", 1111);
+mysqli_select_db($conn, "opentutorials");
+$name = mysqli_real_escape_string($conn, $_GET['name']);
+$name = mysqli_real_escape_string($conn, $_GET['password']);
+$sql = "SELECT * FROM user WHERE name='".$name."' AND password='".$password."'";
+echo $sql;
+$result = mysqli_query($conn, $sql);
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+      if($result->num_rows == "0"){
+        echo "뉘신지?";
+      }else {
+        echo "안녕하세요. 주인님";
+      }
+     ?>
+  </body>
+</html>
